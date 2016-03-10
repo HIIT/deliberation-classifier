4696561209ed6ab83c6ae0b8815cd82a1def8e2f
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2016-03-10 12:42:00 +0200

    Update UI patterns

--- app/main.py
+++ app/main.py
@@ -7,14 +7,19 @@ def index():
     return send_from_directory( 'static' , 'index.html' )
 
 ## NEVER USE FOR REAL ANALYSIS, A TOY EXAMPLE
-@app.route('/api/alpha/dqi', methods=['POST'] )
+@app.route('/api/alpha/dqi/', methods=['POST'] )
 def alpha_analyze():
+
+    from alpha import dqi
+
     if 'text' not in request.form:
         return jsonify(
                     code = 400 ,
                     msg = 'Check your post request has a text attribute'
                 ) , 400
-    return request.form['text']
+
+    jl = dqi.predict( request.form['text'] )[0]
+    return jsonify( { 'value' : jl } )
 
 if __name__ == "__main__":
-    app.run()
+    app.run( debug = True )
